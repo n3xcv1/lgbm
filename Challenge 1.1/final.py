2ee9bee56df1d8a59f6e3a63f343a38ddcd5ae89
n3xcv1 <117036195+n3xcv1@users.noreply.github.com>
2023-04-06 13:11:41 -0300

    Add files via upload

--- Challenge 1.1/final.py
+++ Challenge 1.1/final.py
@@ -6,7 +6,9 @@ class Application():
     def __init__(self):
         self.root = root
         self.tela()
+        self.frames_da_tela()
         root.mainloop()
+# Aqui está o redimensionamento da Janela
     def tela(self):
         self.root.title("LGBM")
         self.root.configure(background='#1c1c1c')
@@ -14,5 +16,14 @@ class Application():
         self.root.resizable(True, True)
         self.root.maxsize(width= 900, height= 700)
         self.root.minsize(width= 500, height= 400)
+# Aqui estão os frames
+    def frames_da_tela(self):
+        self.frame_1 = Frame(self.root)
+        self.frame_1.place(relx= 0.01, rely=0.01, relwidth=0.2, relheight= 0.98)
+
+        self.frame_2 = Frame(self.root)
+        self.frame_2.place(relx=0.25, rely=0.01, relwidth=0.74, relheight=0.98)
+# Aqui está os botões
+
 
 Application()
