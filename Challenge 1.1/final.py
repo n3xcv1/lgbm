84c38bc9b5f51edddc23f5b38462401c36ba81c4
n3xcv1 <117036195+n3xcv1@users.noreply.github.com>
2023-04-06 21:18:41 -0300

    Add files via upload

--- Challenge 1.1/final.py
+++ Challenge 1.1/final.py
@@ -1,6 +1,5 @@
 from tkinter import *
 
-
 root = Tk()
 
 class Application():
@@ -11,51 +10,66 @@ class Application():
         self.criando_botoes()
         root.mainloop()
 
-# Aqui está o redimensionamento da Janela
     def tela(self):
         self.root.title("LGBM")
-        self.root.configure(background='#1c1c1c')
-        self.root.geometry("800x600")
+        self.root.configure(background='black')
+        self.root.geometry("3000x1200")
         self.root.resizable(True, True)
-        self.root.maxsize(width= 900, height= 700)
-        self.root.minsize(width= 500, height= 400)
+        self.root.maxsize(width=900, height=700)
+        self.root.minsize(width=500, height=400)
 
-
-# Aqui estão os frames
     def frames_da_tela(self):
+        self.frame_1 = Frame(self.root, background='#004B57')
+        self.frame_1.place(relx=0.01, rely=0.01, relwidth=0.17, relheight=0.98)
 
-# Aqui está o frame da coluna dos botões
-        self.frame_1 = Frame(self.root)
-        self.frame_1.place(relx= 0.01, rely=0.01, relwidth=0.2, relheight= 0.98)
-
-# Aqui está o frame da visualização das tabelas
-        self.frame_2 = Frame(self.root)
+        self.frame_2 = Frame(self.root, background='#004B57')
         self.frame_2.place(relx=0.25, rely=0.01, relwidth=0.74, relheight=0.98)
 
-# Aqui estão os botões
+    def abrir_janela_tusuarios(self):
+        nova_janela = Frame(self.frame_2, background='black')
+        nova_janela.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+        label = Label(nova_janela, text="Tabela Usuários")
+        label.pack(pady=50)
+
+    def abrir_janela_tsetores(self):
+        nova_janela = Frame(self.frame_2, background='blue')
+        nova_janela.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+        label = Label(nova_janela, text="Tabela Setores")
+        label.pack(pady=50)
+
+    def abrir_janela_cadastro(self):
+        nova_janela = Frame(self.frame_2, background='white')
+        nova_janela.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+        label = Label(nova_janela, text="Tabela Cadastro")
+        label.pack(pady=50)
+
+    def abrir_janela_catalogo(self):
+        nova_janela = Frame(self.frame_2, background='yellow')
+        nova_janela.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+        label = Label(nova_janela, text="Tabela Catalogo")
+        label.pack(pady=50)
+
+    def abrir_janela_config(self):
+        nova_janela = Frame(self.frame_2, background='red')
+        nova_janela.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+        label = Label(nova_janela, text="Tabela Config")
+        label.pack(pady=50)
+
     def criando_botoes(self):
+        self.bt_usuarios = Button(self.frame_1, text="Pontos Usuários", background='#daa520', command=self.abrir_janela_tusuarios)
+        self.bt_usuarios.place(relx=0.019, rely=0.01, relwidth=0.97, relheight=0.07)
 
-# Aqui está o botão da tabela dos usuários
-        self.bt_usuarios =  Button(self.frame_1, text= "Pontos Usuários")
-        self.bt_usuarios.place(relx= 0.019, rely=0.01, relwidth=0.95, relheight= 0.07)
+        self.bt_setores = Button(self.frame_1, text="Pontos Setores", background='#daa520', command=self.abrir_janela_tsetores)
+        self.bt_setores.place(relx=0.019, rely=0.09, relwidth=0.97, relheight=0.07)
 
-# Aqui está o botão da tabela dos setores
-        self.bt_setores = Button(self.frame_1, text= "Pontos Setores")
-        self.bt_setores.place(relx=0.019, rely=0.09, relwidth=0.95, relheight=0.07)
+        self.bt_cadastro = Button(self.frame_1, text="Cadastro de Funcionários", background='#daa520', command=self.abrir_janela_cadastro)
+        self.bt_cadastro.place(relx=0.019, rely=0.17, relwidth=0.97, relheight=0.07)
 
-# Aqui está o botão da tabela do cadastro de funcionários
-        self.bt_cadastro = Button(self.frame_1, text= "Cadastro de Funcionários")
-        self.bt_cadastro.place(relx=0.019, rely=0.17, relwidth=0.95, relheight=0.07)
+        self.bt_catalogo = Button(self.frame_1, text="Catálogo de Phishing", background='#daa520', command=self.abrir_janela_catalogo)
+        self.bt_catalogo.place(relx=0.019, rely=0.25, relwidth=0.97, relheight=0.07)
 
-# Aqui está o botão de catalógo
-        self.bt_catalogo = Button(self.frame_1, text="Catálogo de Phishing")
-        self.bt_catalogo.place(relx=0.019, rely=0.25, relwidth=0.95, relheight=0.07)
-
-# Aqui está o botão de configuracoes
-        self.bt_configuracoes = Button(self.frame_1, text="Configurações")
-        self.bt_configuracoes.place(relx=0.019, rely=0.92, relwidth=0.95, relheight=0.07)
-
-
+        self.bt_configuracoes = Button(self.frame_1, text="Configurações", background='#daa520', command=self.abrir_janela_config)
+        self.bt_configuracoes.place(relx=0.019, rely=0.92, relwidth=0.97, relheight=0.07)
 
 
 Application()
