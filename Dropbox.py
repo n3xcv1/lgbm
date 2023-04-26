fffecef1823a1147574638f9d32e7e29d3e36da8
n3xcv1 <117036195+n3xcv1@users.noreply.github.com>
2023-04-26 16:23:43 -0300

    Add files via upload

---
+++ Dropbox.py
@@ -0,0 +1,83 @@
+<html>
+	  <style>
+	    body {
+	      margin: 0;
+	      padding: 0;
+	      overflow: auto !important;
+	    }
+
+	    ::-webkit-scrollbar {
+	      -webkit-appearance: none;
+	      height: 5px;
+	      width: 5px;
+	    }
+
+	    ::-webkit-scrollbar-thumb {
+	      border-radius: 4px;
+	      background-color: #DCE0E6;
+	    }
+
+	    ::-webkit-scrollbar-corner {
+	      -webkit-appearance: none;
+	    }
+	  </style>
+
+	  <body>
+	    <div dir="ltr">
+	      <div>
+	        <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <img src="data:image/jpeg;base64," alt="2560px-Dropbox_logo_2017.svg.png" width="240" height="47" style="margin-right: 0px;">
+	          <p>
+	            <br><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Assunto: Ação necessária: Redefina sua senha da Dropbox</span>
+	          </p>
+	        </div>
+	        <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Prezado </span><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Bruno Néris Silva</span><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">,</span>
+	          </p>
+	        </div>
+	        <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Notamos uma atividade incomum em sua conta da Dropbox e, por motivos de segurança, estamos solicitando que você redefina sua senha imediatamente. Isso ajudará a proteger sua conta contra acesso não autorizado.</span>
+	          </p>
+	        </div>
+	        <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Por favor, siga as etapas abaixo para redefinir sua senha:</span>
+	          </p>
+	        </div>
+	        <ol style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <li style="list-style:decimal">1. <span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Clique no link abaixo para ir para a página de redefinição de senha:</span>
+	          </li>
+	        </ol>
+	        <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <p>
+	            <a href="#" style="font-size:16px;color:rgb(26,98,255);letter-spacing:0em" data-href="https://sites.google.com/view/concientizao-sobre-phishing/in%C3%ADcio" data-valid-url="true">[Link para a página de redefinição de senha da Dropbox]</a>
+	          </p>
+	        </div>
+	        <ol style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <li style="list-style:decimal">1. <span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Insira seu endereço de e-mail associado à sua conta da Dropbox.</span>
+	          </li>
+	          <li style="list-style:decimal">2. <span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Siga as instruções para criar uma nova senha segura.</span>
+	          </li>
+	        </ol>
+	        <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Se você não solicitou uma redefinição de senha ou acredita que alguém possa ter acessado sua conta, entre em contato conosco imediatamente para que possamos tomar as medidas necessárias para proteger sua conta.</span>
+	          </p>
+	        </div>
+	        <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Obrigado por cooperar conosco para manter sua conta segura.</span>
+	          </p>
+	        </div>
+	        <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Atenciosamente,</span>
+	          </p>
+	        </div>
+	        <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	          <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Equipe de segurança da Dropbox</span>
+	          </p>
+	          <br>
+	          <br>
+	        </div>
+	      </div>
+	    </div>
+	  </body>
+
+	</html>
