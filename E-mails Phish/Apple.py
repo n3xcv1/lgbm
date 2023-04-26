c75a91448adc292e5a1c502202765b76a70af8a3
n3xcv1 <117036195+n3xcv1@users.noreply.github.com>
2023-04-26 16:25:19 -0300

    Add files via upload

---
+++ E-mails Phish/Apple.py
@@ -0,0 +1,76 @@
+<html>
+	  <style>
+	    body {
+	      margin: 0;
+	      padding: 0;
+	      overflow: auto !important;
+	    }
+
+	    ::-webkit-scrollbar {
+	      -webkit-appearance: none;
+	      height: 5px;
+	      width: 5px;
+	    }
+
+	    ::-webkit-scrollbar-thumb {
+	      border-radius: 4px;
+	      background-color: #DCE0E6;
+	    }
+
+	    ::-webkit-scrollbar-corner {
+	      -webkit-appearance: none;
+	    }
+	  </style>
+
+	  <body>
+	    <div dir="ltr">
+	      <img src="data:image/jpeg;base64," alt="Apple-Logo.png" width="108" height="61" style="margin-right: 0px;">
+	      <br>
+	      <div>
+	        <br>
+	      </div>
+	      <div>
+	        <div>
+	          <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	            <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Assunto: Ação necessária: Redefina sua forma de pagamento na Apple</span>
+	            </p>
+	          </div>
+	          <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	            <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Prezado </span><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Mateus Andrade de Souza</span><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">,</span>
+	            </p>
+	          </div>
+	          <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	            <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Recentemente, detectamos um problema com a forma de pagamento associada à sua conta da Apple. Para garantir que suas compras na App Store, iTunes Store e outros serviços da Apple sejam processadas sem interrupções, solicitamos que você atualize suas informações de pagamento o mais rápido possível.</span>
+	            </p>
+	          </div>
+	          <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	            <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Por favor, </span><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">entre no link abaixo para fazer a alteração.</span>
+	            </p>
+	          </div>
+	          <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	            <p>
+	              <a href="#" style="font-size:15.9999px;color:rgb(26,98,255);letter-spacing:0em" data-href="https://sites.google.com/view/concientizao-sobre-phishing/in%C3%ADcio" data-valid-url="true">https://support.apple.com&nbsp;› pt-b</a>
+	            </p>
+	          </div>
+	          <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	            <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Se você precisar de ajuda para atualizar suas informações de pagamento, visite nossa página de suporte ou entre em contato com o suporte da Apple para obter assistência.</span>
+	            </p>
+	          </div>
+	          <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	            <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Lembre-se de que uma forma de pagamento atualizada é necessária para continuar usando nossos serviços sem interrupções. Agradecemos antecipadamente por tomar medidas para garantir que sua conta permaneça ativa e que você possa continuar desfrutando dos serviços da Apple.</span>
+	            </p>
+	          </div>
+	          <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	            <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Atenciosamente,</span>
+	            </p>
+	          </div>
+	          <div style="line-height:1.4;direction:ltr;margin-left:0px">
+	            <p><span style="font-size:16px;color:rgb(0,0,0);letter-spacing:0em">Equipe de suporte da Apple</span>
+	            </p>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+	  </body>
+
+	</html>
